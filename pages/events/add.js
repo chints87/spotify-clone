079423541696582c359ccddd87e2eb80ac57375f
chints87 <cgandhi4@gmail.com>
2021-11-24 23:28:59 +0530

    Add Layout component to pages

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,7 +1,11 @@
+import Layout from "../../components/Layout"
+
 export default function AddEvents() {
     return (
         <div>
-            Add
+            <Layout title="Add Event" description="Add event" keywords="is,a,event">
+                <h1>Add</h1>
+            </Layout>
         </div>
     )
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,9 @@
+import Layout from "../../components/Layout"
+
 export default function EventsPage() {
     return (
-        <div>
-            Events
-        </div>
+        <Layout title="Events Page" description="events" keywords="is,a,event" >
+            <h1>Events</h1>
+        </Layout>
     )
 }
