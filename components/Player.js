ab804fbd0f6472b2a8bc08a9e27aeadfbc157af5
Chintan <cgandhi4@gmail.com>
2022-01-18 19:44:44 +0530

    Add Player

---
+++ components/Player.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/scss/Player.module.scss';
+
+export default function Player() {
+  return (
+    <div className={styles.player}>
+      This is the player
+    </div>
+  );
+}
