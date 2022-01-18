64e6e5560857b86b4fcb7dbb87db2cb8fbcfeab7
Chintan <cgandhi4@gmail.com>
2022-01-18 18:10:43 +0530

    Add Sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,46 @@
+import { HomeIcon, HeartIcon, SearchIcon, LibraryIcon, PlusCircleIcon, RssIcon } from '@heroicons/react/outline';
+import styles from '@/styles/scss/Sidebar.module.scss';
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sideBar}>
+      <button type="button" className="primary">
+        <div className={styles.buttonContent}>
+          <HomeIcon className="iconSize" />
+          <p>Home</p>
+        </div>
+      </button>
+      <button type="button" className="primary">
+        <div className={styles.buttonContent}>
+          <SearchIcon className="iconSize" />
+          <p>Search</p>
+        </div>
+      </button>
+      <button type="button" className="primary">
+        <div className={styles.buttonContent}>
+          <LibraryIcon className="iconSize" />
+          <p>Your Library</p>
+        </div>
+      </button>
+      <hr />
+      <button type="button" className="primary">
+        <div className={styles.buttonContent}>
+          <PlusCircleIcon className="iconSize" />
+          <p>Create Playlist</p>
+        </div>
+      </button>
+      <button type="button" className="primary">
+        <div className={styles.buttonContent}>
+          <HeartIcon className="iconSize" />
+          <p>Your Songs</p>
+        </div>
+      </button>
+      <button type="button" className="primary">
+        <div className={styles.buttonContent}>
+          <RssIcon className="iconSize" />
+          <p>Your Episodes</p>
+        </div>
+      </button>
+    </div>
+  );
+}
