645a9f1c3432be0829cff67a02037525e3673ef9
Chintan <cgandhi4@gmail.com>
2022-01-20 17:57:39 +0530

    Add Session Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
-import '../styles/scss/globals.scss'
+import '../styles/scss/globals.scss';
+import { SessionProvider } from 'next-auth/react';
 
 export function reportWebVitals(metric) {
-  console.log(metric)
+  console.log(metric);
 }
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return (
+    // allows instances of useSession() to share the session object across components,
+    // by using React Context under the hood
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
