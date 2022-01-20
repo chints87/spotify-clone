2e1a575bc1d51acc20cb234438d84d3990782b71
Chintan <cgandhi4@gmail.com>
2022-01-20 17:55:03 +0530

    Update components

--- components/Player.js
+++ components/Player.js
@@ -3,7 +3,55 @@ import styles from '@/styles/scss/Player.module.scss';
 export default function Player() {
   return (
     <div className={styles.player}>
-      This is the player
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
+      <div>Playlist</div>
     </div>
   );
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,16 @@
 import {
   HomeIcon, HeartIcon, SearchIcon, LibraryIcon, PlusCircleIcon, RssIcon,
 } from '@heroicons/react/outline';
+import { useSession, signOut } from 'next-auth/react';
 import styles from '@/styles/scss/Sidebar.module.scss';
 
 export default function Sidebar() {
+  const { data: session, status } = useSession();
+  console.log('in sidebar', status);
+  console.log('in sidebar', session);
   return (
     <div className={styles.sideBar}>
-      <button type="button" className="primary">
+      <button type="button" className="primary" onClick={() => signOut()}>
         <div className={styles.buttonContent}>
           <HomeIcon className="iconSize" />
           <p>Home</p>
@@ -24,7 +28,10 @@ export default function Sidebar() {
           <p>Your Library</p>
         </div>
       </button>
-      <hr />
+      <hr style={{
+        border: '1', color: 'grey',
+      }}
+      />
       <button type="button" className="primary">
         <div className={styles.buttonContent}>
           <PlusCircleIcon className="iconSize" />
