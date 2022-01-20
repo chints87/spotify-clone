ae851b837fa8cb6c801c5dc8f21ae3841e561cb6
Chintan <cgandhi4@gmail.com>
2022-01-20 17:56:02 +0530

    Add Components Center and Layout

---
+++ components/Center.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '@/styles/scss/Center.module.scss';
+
+export default function Center() {
+  return (
+    <div className={styles.center}>This is center</div>
+   
+  );
+}
